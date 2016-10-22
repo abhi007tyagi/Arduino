7ffbeaa38da7512170568b2dd74b3d1721824181
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2016-10-22 22:25:01 +0530

    initial commit with blinking leds and LDR experiment 1 projects

---
+++ sketch_LDR_1/sketch_LDR_1.ino
@@ -0,0 +1,23 @@
+int LDR = A0;
+int LDRValue = 0;
+int lightSensitivity = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+//  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  LDRValue = analogRead(LDR);
+//  Serial.print("LDR value is: ");
+//  Serial.println(LDRValue);
+  delay(50);
+
+  if(LDRValue < lightSensitivity){
+    digitalWrite(13, HIGH);
+  } else{
+    digitalWrite(13, LOW);
+  }
+}
