bf0341e7e3a63ff233b3c7ba00e5a7f17c19ef61
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-28 00:25:04 +0530

    test blink code added

--- sketch_LM35_Temp_Sensor/sketch_LM35_Temp_Sensor.ino
+++ sketch_LM35_Temp_Sensor/sketch_LM35_Temp_Sensor.ino
@@ -10,6 +10,6 @@ Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
 void loop()
 {
 tempC = (5.0 * analogRead(tempPin) * 100.0)/1024.0;  //convert the analog data from sensor to temperature
-Serial.print((byte)tempC);             //send the data to the computer for logging
+Serial.print((byte)tempC);             //send the temp (in celcius) to the computer for logging
 delay(1000);                           //wait one second
 }
